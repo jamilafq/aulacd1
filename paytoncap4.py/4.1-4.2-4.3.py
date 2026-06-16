70123b5fc8ec7bb273692da39d22fe405a03595b
Jamila Queiroz <jamilafq@gmail.com>
2026-06-16 16:06:40 -0300

    Create 4.1-4.2-4.3.py

---
+++ paytoncap4.py/4.1-4.2-4.3.py
@@ -0,0 +1,97 @@
+# 4.1
+
+# a= int(input("primeiro valor:" ))
+# b = int (input ("segundo valor "))
+# if a >b:
+#     print ("o primeniro é o maior !")
+# if  b>a :
+#     print ("o segundo valor é o maior! ")
+
+#4.2 exemplo carro novo ou carro velho,  depende da idade 
+
+# idade= int (input("Digidit a didade do seu carro:"))
+# if idade <=3:
+#     print ("seu carro e novo")
+# if idade >3:
+#     print ("seu carro é velhor ")
+
+#4.2 Pergunte a velocidade do carro ?/ Ultrapasse 80 km/h,  exiba  a mensagem que o usuario foi multado/ 5 por km acima de 80km/h
+
+# velocidade = int(input("Qual a velocidade dos carro? "))
+
+# if velocidade > 80:
+#     print (f"Usuario foi multado")
+
+# if velocidade <= 80 :
+#     print(f"bom motorista")    
+
+# 4.3 Escreva um programa que leia tres números e que imprima o maior e o menor.  
+
+
+
+# a = float (input('digite um numero '))
+# b= float (input("digite um numro "))
+# c= float (input ("digite um numero "))
+ 
+# maior = a  
+
+# if b> maior :   
+#     maior = b 
+# if c > maior :
+#     maior = c 
+
+# print (f"O maior numero é:  {maior}") 
+
+
+#4.4 Perergunte o salário do funcinário e calcule o valor do aumento. / Superirores a 1.250 calculoe um aumento de 10% e para os inferiores ou iguais ,  de 15%
+
+# salario = float(input("digite o seu salario"))
+# if salario >1250:  
+#   aumento =  salario * 0.1
+
+# if salario <= 1250:
+#     aumento = salario *0.15
+# print(f"Seu aumento foi de: R$ {aumento:.2f}")
+# print(f"Novo salário: R$ {salario + aumento:.2f}")
+
+
+
+# 4.6 Qual a distancia que um passageiro deseja percorrer em Km.  Calcule o preço da passagem./ 0,50 POR Km para viagens de até 200Km  e  0,45 para viagens mais longas 
+
+# distancia = float(input( "Qual a distancia percorrida"))
+
+# if distancia <= 200:
+#     aumento = distancia *0.05
+# else:  
+#    aumento = distancia *0.045
+
+# print (f"O valor do percurso é de : {aumento: 2f}")
+
+
+# # 4.6 ex: CATEGORIA X PREÇO 
+# Categoria = int (input ("Digite a categoria do produto "))
+
+# if Categoria == 1:
+#     preço = 10
+# else : 
+#     if Categoria == 2:
+#         preço = 18
+#     else :
+#         if Categoria ==3:
+#             preço = 23
+#         else :
+#             if Categoria==4:
+#                 preço =26
+
+#             else:
+#                 if Categoria == 5:
+#                     preço =26
+#                 else :
+#                     printe ("Categoria inválida ,  digit um valor entre 1 e 5! ")   
+# print (f"O preço do  produto é : R${preço: 6.2f} ")                     
+
+                
+
+
+
+
