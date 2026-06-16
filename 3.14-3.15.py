4407f4f2c3fe2edfb2ba90b992cf11ae54ab9df2
Jamila Queiroz <jamilafq@gmail.com>
2026-06-16 11:47:34 -0300

    3.10

---
+++ 3.14-3.15.py
@@ -0,0 +1,20 @@
+# 3.14 qual a quantidade de km percorrido?/ quantidade de dias?/ Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$ 0,15 por km rodado. 
+
+# km = float (input(' Qual a distancia percorrida ?'))
+# dias = float(input("Quantos dias de aluguel? "))
+
+# totalDias = dias * 60
+# totalKm = km *0.15
+
+# precoPagar = totalDias + totalKm
+
+# print (f"O preço a ser pago é de : {precoPagar}")
+
+# 3.15 calcular a reduçao do tempo de vida de um fumante./ Quantidade de cigarro fumados por dia ?/ Um fumante perde 10 minutos de vida a cada cigarro. / Calcule quantos dias de vida um fumante perderá.  Exiba o total de dias.  
+
+# cigarros = float(input("Qual a quantidade de cigarros fumados por dia? "))
+
+# minutos_perdidos = cigarros * 10
+# dias_perdidos = minutos_perdidos / (24 * 60)
+
+# print(f"Total de dias perdidos: {dias_perdidos:.2f}")
---
+++ pythoncap3.py/exercicio31.py/3.10-3.11-3.12-3.13.py
@@ -0,0 +1,35 @@
+# 3.10 aumento de salario/ ele dever solicitar o valor do salário e a procentagem.
+
+
+# salario = float(input("qual o seu salario? "))
+# porcentagem = float(input('Qual a porcentagem do aumento ? '))
+# aumento = salario + salario * porcentagem / 100
+# novoSalario= aumento + salario
+# print (f" Aumento foi de {aumento: 2f}")
+# print (f" Novo salario {novoSalario: 2f}")
+
+
+#3.11 solicite o preço de uma mercadoria  eo percentual  de desconto.  Exiba o valor de desconto eo preço a pagar 
+
+# produto = float(input("qual preço do produto? "))
+# porcentagem = float(input('Qual a porcentagem de desconto ? '))
+# desconto =  produto * porcentagem / 100
+# novoPreco= desconto-produto 
+# print (f" Desconto foi de {desconto : 2f}")
+# print (f" Novo produto {novoPreco: 2f}")
+
+
+# 3.12 escreva um programa que calcule o tempo de uma viagem de carro.  Pergunte a distâcia percorrida e valocidade média esperada  da viagem 
+
+# distancia = float (input("qual a distancia percorrida?"))
+# velocidade = float (input("qual a velocidade? "))
+# tempo = distancia / velocidade
+
+# print(f"Tempo: {tempo} horas")
+
+
+#3.13 Escreva um programa que converta uma temperatura digitade em ºC EM ºF. A fórmula para essa conversão é :  (9*c)/5 +32
+
+temperatura = float (input('Qual a temperatura?  '))
+tempF =  (9*temperatura)/5 +32
+print (f"tempreratura em Fº é: {tempF}")
