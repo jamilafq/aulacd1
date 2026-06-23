8c8096e68ae755714c68716870bb97947a3ee96e
Jamila Queiroz <jamilafq@gmail.com>
2026-06-23 16:06:30 -0300

    cap5

---
+++ cap5.py/5.1-5.2-5.3.py
@@ -0,0 +1,33 @@
+# x= 1
+# while x <=3:
+#     print (x)
+#     x= x+1
+
+# 5.1 modifique o programa acima para exibir os numeros de um a 100
+# x= 1
+# while x <=100:
+#      print (x)
+#      x= x+1
+
+
+# modifique o programa acima para exibir os numeros de 50 a 100
+# x= 50
+# while x <=100:
+#     print (x)
+#     x= x+1
+
+
+# faça um program para escrever a contagem regressiva do lançamento de um foguete. O programa deve imprimir 10,9,8,7...0 e fogo na tela 
+
+# x = 10
+# while x >= 0:
+#     print (x)
+#     x = x-1
+# print ('fogo')
+
+# fim= int(input("digite o ultimo numero a imprimir : "))
+# x=1
+# while x<= fim :
+#     print(x)
+#     x= x+1
+
---
+++ cap5.py/5.4-5.5-5.6-5.7-5.8-5.9.py
@@ -0,0 +1,7 @@
+# 5.4 Modifique o programa anterior para imprimir de 1 ate o numero digitado pelo usuario,  mas dessa vez apenas numeros pares 
+fim = int(input("Digite o ultimo numero a ser contado: "))
+x = 1 
+while x >= fim:
+  if x % 2 == 0:
+    print (x)
+  x =  x + 1
