fa200920cd3e9720629c13caea56fe4869ebdfd0
Jamila Queiroz <jamilafq@gmail.com>
2026-06-23 16:26:29 -0300

    Update 5.4-5.5-5.6-5.7-5.8-5.9.py

--- cap5.py/5.4-5.5-5.6-5.7-5.8-5.9.py
+++ cap5.py/5.4-5.5-5.6-5.7-5.8-5.9.py
@@ -1,7 +1,9 @@
 # 5.4 Modifique o programa anterior para imprimir de 1 ate o numero digitado pelo usuario,  mas dessa vez apenas numeros pares 
 fim = int(input("Digite o ultimo numero a ser contado: "))
-x = 1 
-while x >= fim:
-  if x % 2 == 0:
-    print (x)
-  x =  x + 1
+
+x = 1
+
+while x <= fim:
+    if x % 2 == 0:
+        print(x)
+    x = x + 1
